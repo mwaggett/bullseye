cde9c4dc152acf9c09105e1a7da4448c7710bb14
Molly Waggett <mollywaggett@AppTreeDevSever.local>
2015-11-24 14:21:18 -0800

    Style app.

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
 
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
