123d1a9c3b44a7b490eceb7ef0ffcd56e13b04bc
Molly Waggett <mollywaggett@AppTreeDevSever.local>
2015-11-24 12:24:09 -0800

    Add 'About' screen.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Molly Waggett on 11/24/15.
+//  Copyright © 2015 Molly Waggett. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
