51ebf77bd21b7250a901e9683aedd8c5ca619ec8
Molly Waggett <mollywaggett@AppTreeDevSever.local>
2015-11-24 10:41:17 -0800

    Display value of slider in alert when 'Hit Me' button is pressed.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 50;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +22,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(title: "Hello, World",
-                                    message: "This is my first app!",
+                                    message: message,
                                     preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Awesome", style: .Default,
+        let action = UIAlertAction(title: "OK", style: .Default,
                                     handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }
 
