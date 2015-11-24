63d56c141627bd4172bf7a731fbbb546ded3996d
Molly Waggett <mollywaggett@AppTreeDevSever.local>
2015-11-24 11:08:18 -0800

    Generate random target value and display it to the user at the start of each new round.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50;
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    var targetValue: Int = 0;
+    var currentValue: Int = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +27,7 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World",
                                     message: message,
                                     preferredStyle: .Alert)
@@ -30,10 +35,23 @@ class ViewController: UIViewController {
                                     handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
