d9f910bfaf4bdf573e8595b5b497083df8c31ed7
Molly Waggett <mollywaggett@AppTreeDevSever.local>
2015-11-24 11:30:20 -0800

    Calculate and round score to user in alert, update total score, and increment round number.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
-    var targetValue: Int = 0;
-    var currentValue: Int = 0;
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +30,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
-                    + "\nThe target value is: \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let message = "You scored \(points) points!"
         let alert = UIAlertController(title: "Hello, World",
                                     message: message,
                                     preferredStyle: .Alert)
@@ -48,10 +55,13 @@ class ViewController: UIViewController {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        round++
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
